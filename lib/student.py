964279df494157f3ed5f562f69626fc8ff9e69da
waltersimekha <waltersimekha@gmail.com>
2024-03-12 10:02:20 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
-
-from user import User
+# lib/student.py
+from lib.user import User  # Correct the import statement
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+
+        self.knowledge.append(new_knowledge)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,34 @@
-#!/usr/bin/env python
 
-from user import User
 
+# lib/teacher.py
+from lib.user import User  # Fix the import statement
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+
+
+
+class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+    def teach(self):
+        return random.choice(self.knowledge)
